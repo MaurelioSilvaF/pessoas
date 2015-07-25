b65ddf96a79c98770994f91ae50b29b9fd3393fd
MaurelioSilvaF <maureliosilvaf@gmail.com>
2015-07-25 19:07:23 -0300

    Changed Poduction DataBase

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150725192828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "adminpack"
 
   create_table "pessoas", force: true do |t|
     t.string   "name"
